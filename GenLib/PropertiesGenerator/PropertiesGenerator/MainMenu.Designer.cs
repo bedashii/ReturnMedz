328e246434ed936ab82b88cf1f5b7f72daeb60b8
Lance Bedasie <lance@holistics.co.za>
2012-06-27 11:05:10 +0200

    Added the Properties Generator into folder GenLib

---
+++ GenLib/PropertiesGenerator/PropertiesGenerator/MainMenu.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace PropertiesGenerator
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGVProperties = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TBoxClassName = new System.Windows.Forms.TextBox();
+            this.ButtonGenerate = new System.Windows.Forms.Button();
+            this.ButtonReset = new System.Windows.Forms.Button();
+            this.PropertyName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DataType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.VariableSize = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nullable = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVProperties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGVProperties
+            // 
+            this.DGVProperties.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.DGVProperties.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DGVProperties.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVProperties.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.PropertyName,
+            this.DataType,
+            this.VariableSize,
+            this.Nullable});
+            this.DGVProperties.Location = new System.Drawing.Point(0, 82);
+            this.DGVProperties.Name = "DGVProperties";
+            this.DGVProperties.Size = new System.Drawing.Size(410, 369);
+            this.DGVProperties.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Class Name";
+            // 
+            // TBoxClassName
+            // 
+            this.TBoxClassName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.TBoxClassName.Location = new System.Drawing.Point(81, 42);
+            this.TBoxClassName.Name = "TBoxClassName";
+            this.TBoxClassName.Size = new System.Drawing.Size(317, 20);
+            this.TBoxClassName.TabIndex = 2;
+            this.TBoxClassName.Validating += new System.ComponentModel.CancelEventHandler(this.TBoxClassName_Validating);
+            // 
+            // ButtonGenerate
+            // 
+            this.ButtonGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonGenerate.Location = new System.Drawing.Point(323, 12);
+            this.ButtonGenerate.Name = "ButtonGenerate";
+            this.ButtonGenerate.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGenerate.TabIndex = 3;
+            this.ButtonGenerate.Text = "Generate";
+            this.ButtonGenerate.UseVisualStyleBackColor = true;
+            this.ButtonGenerate.Click += new System.EventHandler(this.ButtonGenerate_Click);
+            // 
+            // ButtonReset
+            // 
+            this.ButtonReset.Location = new System.Drawing.Point(12, 12);
+            this.ButtonReset.Name = "ButtonReset";
+            this.ButtonReset.Size = new System.Drawing.Size(75, 23);
+            this.ButtonReset.TabIndex = 3;
+            this.ButtonReset.Text = "Reset";
+            this.ButtonReset.UseVisualStyleBackColor = true;
+            this.ButtonReset.Click += new System.EventHandler(this.ButtonReset_Click);
+            // 
+            // PropertyName
+            // 
+            this.PropertyName.HeaderText = "Property Name";
+            this.PropertyName.Name = "PropertyName";
+            // 
+            // DataType
+            // 
+            this.DataType.HeaderText = "Data Type";
+            this.DataType.Name = "DataType";
+            // 
+            // VariableSize
+            // 
+            this.VariableSize.HeaderText = "Variable Size";
+            this.VariableSize.Name = "VariableSize";
+            // 
+            // Nullable
+            // 
+            this.Nullable.HeaderText = "Nullable";
+            this.Nullable.Name = "Nullable";
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 451);
+            this.Controls.Add(this.ButtonReset);
+            this.Controls.Add(this.ButtonGenerate);
+            this.Controls.Add(this.TBoxClassName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DGVProperties);
+            this.Name = "MainMenu";
+            this.Text = "MainMenu";
+            ((System.ComponentModel.ISupportInitialize)(this.DGVProperties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGVProperties;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TBoxClassName;
+        private System.Windows.Forms.Button ButtonGenerate;
+        private System.Windows.Forms.Button ButtonReset;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PropertyName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DataType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn VariableSize;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Nullable;
+    }
+}
---
+++ GenLib/PropertiesGenerator/PropertiesGenerator/MainMenu.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PropertiesGenerator
+{
+    public partial class MainMenu : Form
+    {
+        string _className;
+
+        struct Variables
+        {
+            public string Name;
+            public string Type;
+            public int VariableSize;
+            public bool AllowNulls;
+        }
+
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonGenerate_Click(object sender, EventArgs e)
+        {
+            List<Variables> vList = new List<Variables>();
+            
+            for (int i = 0; i < DGVProperties.Rows.Count - 1; i++)
+            {
+                Variables v = new Variables();
+                v.Name =  DGVProperties.Rows[i].Cells[0].Value.ToString();
+                v.Type = DGVProperties.Rows[i].Cells[1].Value.ToString();
+                v.VariableSize = Convert.ToInt32(DGVProperties.Rows[i].Cells[2].Value.ToString());
+                v.AllowNulls = Convert.ToBoolean(DGVProperties.Rows[i].Cells[3].Value);
+                vList.Add(v);
+            }
+
+            WriteProperties(vList);
+            WriteData(vList);
+        }
+
+        private void WriteData(List<Variables> vList)
+        {
+            PropertiesGenerator.DataProperties dp = new PropertiesGenerator.DataProperties(System.Configuration.ConfigurationManager.AppSettings["DataNamespace"].ToString(), _className);
+
+            foreach (Variables v in vList)
+            {
+                PropertiesGenerator.DataProps dProps = new PropertiesGenerator.DataProps();
+                dProps.PName = v.Name;
+                dProps.PDataType = v.Type;
+                dProps.CanBeNull = v.AllowNulls;
+                dProps.Size = v.VariableSize;
+                dp.Prop.Add(dProps);
+            }
+
+            StreamWriter sw = new StreamWriter(dp.FileName);
+            sw.Write(dp.GetAll);
+            sw.Close();
+        }
+
+        private void WriteProperties(List<Variables> vList)
+        {
+            PropertiesGenerator.PropertiesProperties pp = new PropertiesGenerator.PropertiesProperties(System.Configuration.ConfigurationManager.AppSettings["PropertiesNamespace"].ToString(), _className);
+
+            foreach (Variables v in vList)
+            {
+                PropertiesGenerator.PropertiesProps pProps = new PropertiesGenerator.PropertiesProps();
+                pProps.PName = v.Name;
+                pProps.PDataType = v.Type;
+                pProps.AllowNulls = v.AllowNulls;
+                pp.Prop.Add(pProps);
+            }
+
+            StreamWriter sw = new StreamWriter(pp.FileName);
+            sw.Write(pp.GetAll);
+            sw.Close();
+        }
+
+        private void ButtonReset_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TBoxClassName_Validating(object sender, CancelEventArgs e)
+        {
+            _className = TBoxClassName.Text;
+        }
+    }
+}
