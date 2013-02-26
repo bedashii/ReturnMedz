c5eb95be8b36ab348ef6fba8883742c64b4078d1
Rian <Rianm@Holistics.co.za>
2013-02-26 21:44:36 +0200

    DownloadSorter in working condition. Delete Confirmation screen added.

---
+++ MedzRandom/DownloadSorter/DownloadSorter/CustomFile.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace DownloadSorter
+{
+    public class CustomFile
+    {
+        public string FileName { get; set; }
+        public string Extention { get; set; }
+        public string FullPath { get; set; }
+        //public string DestinationFileName { get; set; }
+        public long Size { get; set; }
+        public string DestinationFullPath { get; set; }
+        public string ParentName { get; set; }
+        //public CustomFile()
+        //{
+
+        //}
+
+        public CustomFile(string fullPath)
+        {
+            if (File.Exists(fullPath))
+            {
+                FullPath = fullPath;
+                FileInfo details = new FileInfo(fullPath);
+                Extention = details.Extension;
+                Size = details.Length / 1024 / 1024;
+
+                getParentName();
+            }
+            else
+            {
+
+            }
+        }
+
+        private void getParentName()
+        {
+            List<int> hierList = new List<int>();
+            hierList.Add(FullPath.IndexOf('\\'));
+            while (hierList.Last() != -1)
+            {
+                hierList.Add(FullPath.IndexOf('\\', hierList.Last() + 1));
+            }
+            ParentName = FullPath.Substring(hierList[hierList.Count - 3] + 1, hierList[hierList.Count - 2] - hierList[hierList.Count - 3] - 1);
+        }
+    }
+}
--- MedzRandom/DownloadSorter/DownloadSorter/Form1.Designer.cs
+++ MedzRandom/DownloadSorter/DownloadSorter/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DownloadSorter
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
@@ -36,17 +36,26 @@
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.selectAllOfThisTypeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteSelectedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label2 = new System.Windows.Forms.Label();
             this.textBoxDestination = new System.Windows.Forms.TextBox();
             this.buttonMove = new System.Windows.Forms.Button();
             this.checkBoxUseFolderName = new System.Windows.Forms.CheckBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteAllEmptyFoldersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearAnnoyancesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findSamplesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Location = new System.Drawing.Point(12, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 1;
@@ -56,7 +65,7 @@
             // 
             this.textBoxPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxPath.Location = new System.Drawing.Point(55, 12);
+            this.textBoxPath.Location = new System.Drawing.Point(55, 27);
             this.textBoxPath.Name = "textBoxPath";
             this.textBoxPath.Size = new System.Drawing.Size(662, 20);
             this.textBoxPath.TabIndex = 2;
@@ -65,7 +74,7 @@
             // buttonScan
             // 
             this.buttonScan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonScan.Location = new System.Drawing.Point(723, 12);
+            this.buttonScan.Location = new System.Drawing.Point(723, 27);
             this.buttonScan.Name = "buttonScan";
             this.buttonScan.Size = new System.Drawing.Size(75, 23);
             this.buttonScan.TabIndex = 3;
@@ -76,12 +85,14 @@
             // buttonCopy
             // 
             this.buttonCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCopy.Enabled = false;
             this.buttonCopy.Location = new System.Drawing.Point(723, 454);
             this.buttonCopy.Name = "buttonCopy";
             this.buttonCopy.Size = new System.Drawing.Size(75, 23);
             this.buttonCopy.TabIndex = 4;
             this.buttonCopy.Text = "Copy";
             this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Visible = false;
             this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
             // 
             // treeView1
@@ -91,17 +102,20 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.treeView1.CheckBoxes = true;
             this.treeView1.ContextMenuStrip = this.contextMenuStrip1;
-            this.treeView1.Location = new System.Drawing.Point(12, 38);
+            this.treeView1.Location = new System.Drawing.Point(12, 56);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(786, 410);
+            this.treeView1.Size = new System.Drawing.Size(786, 392);
             this.treeView1.TabIndex = 5;
+            this.treeView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseDown);
             // 
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.selectAllOfThisTypeToolStripMenuItem});
+            this.selectAllOfThisTypeToolStripMenuItem,
+            this.deleteSelectedToolStripMenuItem,
+            this.openFileDirectoryToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(186, 26);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(186, 70);
             // 
             // selectAllOfThisTypeToolStripMenuItem
             // 
@@ -109,6 +123,20 @@
             this.selectAllOfThisTypeToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
             this.selectAllOfThisTypeToolStripMenuItem.Text = "Select all of this Type";
             this.selectAllOfThisTypeToolStripMenuItem.Click += new System.EventHandler(this.selectAllOfThisTypeToolStripMenuItem_Click);
+            // 
+            // deleteSelectedToolStripMenuItem
+            // 
+            this.deleteSelectedToolStripMenuItem.Name = "deleteSelectedToolStripMenuItem";
+            this.deleteSelectedToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.deleteSelectedToolStripMenuItem.Text = "Delete Selected";
+            this.deleteSelectedToolStripMenuItem.Click += new System.EventHandler(this.deleteSelectedToolStripMenuItem_Click);
+            // 
+            // openFileDirectoryToolStripMenuItem
+            // 
+            this.openFileDirectoryToolStripMenuItem.Name = "openFileDirectoryToolStripMenuItem";
+            this.openFileDirectoryToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.openFileDirectoryToolStripMenuItem.Text = "Open File Directory";
+            this.openFileDirectoryToolStripMenuItem.Click += new System.EventHandler(this.openFileDirectoryToolStripMenuItem_Click);
             // 
             // label2
             // 
@@ -154,11 +182,61 @@
             this.checkBoxUseFolderName.Text = "Use FolderName";
             this.checkBoxUseFolderName.UseVisualStyleBackColor = true;
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(810, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.deleteAllEmptyFoldersToolStripMenuItem,
+            this.clearAnnoyancesToolStripMenuItem,
+            this.findSamplesToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // deleteAllEmptyFoldersToolStripMenuItem
+            // 
+            this.deleteAllEmptyFoldersToolStripMenuItem.Name = "deleteAllEmptyFoldersToolStripMenuItem";
+            this.deleteAllEmptyFoldersToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.deleteAllEmptyFoldersToolStripMenuItem.Text = "Delete all empty folders";
+            this.deleteAllEmptyFoldersToolStripMenuItem.Click += new System.EventHandler(this.deleteAllEmptyFoldersToolStripMenuItem_Click);
+            // 
+            // clearAnnoyancesToolStripMenuItem
+            // 
+            this.clearAnnoyancesToolStripMenuItem.Name = "clearAnnoyancesToolStripMenuItem";
+            this.clearAnnoyancesToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.clearAnnoyancesToolStripMenuItem.Text = "Clear Annoyances";
+            this.clearAnnoyancesToolStripMenuItem.Click += new System.EventHandler(this.clearAnnoyancesToolStripMenuItem_Click);
+            // 
+            // findSamplesToolStripMenuItem
+            // 
+            this.findSamplesToolStripMenuItem.Name = "findSamplesToolStripMenuItem";
+            this.findSamplesToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.findSamplesToolStripMenuItem.Text = "Find Samples";
+            this.findSamplesToolStripMenuItem.Click += new System.EventHandler(this.findSamplesToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(198, 22);
+            this.toolStripMenuItem1.Text = "Auto";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(810, 489);
+            this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.checkBoxUseFolderName);
             this.Controls.Add(this.buttonMove);
             this.Controls.Add(this.textBoxDestination);
@@ -168,10 +246,13 @@
             this.Controls.Add(this.buttonScan);
             this.Controls.Add(this.textBoxPath);
             this.Controls.Add(this.label1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "NZB Helper";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -190,6 +271,14 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem selectAllOfThisTypeToolStripMenuItem;
         private System.Windows.Forms.CheckBox checkBoxUseFolderName;
+        private System.Windows.Forms.ToolStripMenuItem deleteSelectedToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteAllEmptyFoldersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearAnnoyancesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem findSamplesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileDirectoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
     }
 }
 
