b70714242f2b81032cba96275310b24439d21b81
Rian Mostert <Rianmostert@Hotmail.com>
2013-07-24 12:06:46 +0200

    Chatters Website
    Added Logo

--- MedzRandom/Chatters/Chatters/ChattersMaster.Master.designer.cs
+++ MedzRandom/Chatters/Chatters/ChattersMaster.Master.designer.cs
@@ -22,6 +22,15 @@ namespace Chatters {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// imageHeader control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imageHeader;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
