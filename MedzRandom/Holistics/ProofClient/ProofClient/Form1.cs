020d1982f278da9f94a8fc050b94b5cb01a51069
Rian Mostert <Rianmostert@Hotmail.com>
2013-07-29 13:45:17 +0200

    Proof Client-Server

---
+++ MedzRandom/Holistics/ProofClient/ProofClient/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProofClient
+{
+    public partial class Form1 : Form
+    {
+        private ClientProcessor _clientProcessor;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _clientProcessor = new ClientProcessor();
+            _clientProcessor.MessageReceived += _clientProcessor_MessageReceived;
+        }
+
+        void _clientProcessor_MessageReceived(string message)
+        {
+            textBox2.Text += message + Environment.NewLine;
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                if (string.IsNullOrEmpty(textBoxServerIP.Text))
+                {
+                    _clientProcessor.SendMessage(textBox1.Text);
+                }
+                else
+                {
+                    _clientProcessor.SendMessage(textBoxServerIP.Text, textBox1.Text);
+                }
+
+            }
+        }
+    }
+}
