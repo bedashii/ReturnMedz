fa9dfda084a71901a5c690c3fc67be754b4aaa41
Rian Mostert <Rianmostert@Hotmail.com>
2013-07-29 13:44:55 +0200

    CRMLightSwitch Test

---
+++ MedzRandom/Holistics/CRMLightSwitch/CRMLightSwitch/Client/UserCode/CompanyDetail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+
+namespace LightSwitchApplication
+{
+    public partial class CompanyDetail
+    {
+        partial void Company_Loaded(bool succeeded)
+        {
+            // Write your code here.
+            this.SetDisplayNameFromEntity(this.Company);
+        }
+
+        partial void Company_Changed()
+        {
+            // Write your code here.
+            this.SetDisplayNameFromEntity(this.Company);
+        }
+
+        partial void CompanyDetail_Saved()
+        {
+            // Write your code here.
+            this.SetDisplayNameFromEntity(this.Company);
+        }
+    }
+}
---
+++ MedzRandom/Holistics/CRMLightSwitch/CRMLightSwitch/Client/UserCode/problemDetail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Presentation.Extensions;
+
+namespace LightSwitchApplication
+{
+    public partial class problemDetail
+    {
+        partial void problem_Loaded(bool succeeded)
+        {
+            // Write your code here.
+            this.SetDisplayNameFromEntity(this.problem);
+        }
+
+        partial void problem_Changed()
+        {
+            // Write your code here.
+            this.SetDisplayNameFromEntity(this.problem);
+        }
+
+        partial void problemDetail_Saved()
+        {
+            // Write your code here.
+            this.SetDisplayNameFromEntity(this.problem);
+        }
+    }
+}
