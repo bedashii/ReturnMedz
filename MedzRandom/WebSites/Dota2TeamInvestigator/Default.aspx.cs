21b1f028718033fb459c615ad011f32fa4bcd6d3
Rian Mostert <Rianmostert@Hotmail.com>
2013-08-19 15:51:17 +0200

    Dota 2 Team Investigator Started

---
+++ MedzRandom/WebSites/Dota2TeamInvestigator/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            PanelPlayers.Controls.Clear();
+            PlayerControl pc;
+            for (int i = 0; i < 5; i++)
+            {
+                pc = Page.LoadControl("~/PlayerControl.ascx") as PlayerControl;
+                if (pc != null)
+                    PanelPlayers.Controls.Add(pc);
+                pc = null;
+            }
+        }
+    }
+}
---
+++ MedzRandom/WebSites/Dota2TeamInvestigator/MatchControl.ascx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MatchControl : System.Web.UI.UserControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ImageButtonHero.ImageUrl = @"http://media.steampowered.com/apps/dota2/images/heroes/elder_titan_sb.png";
+
+        ImageButtonItem1.ImageUrl = @"http://media.steampowered.com/apps/dota2/images/items/recipe_rapier_lg.png";
+        ImageButtonItem2.ImageUrl = @"http://media.steampowered.com/apps/dota2/images/items/recipe_rapier_lg.png";
+        ImageButtonItem3.ImageUrl = @"http://media.steampowered.com/apps/dota2/images/items/recipe_rapier_lg.png";
+        ImageButtonItem4.ImageUrl = @"http://media.steampowered.com/apps/dota2/images/items/recipe_rapier_lg.png";
+        ImageButtonItem5.ImageUrl = @"http://media.steampowered.com/apps/dota2/images/items/recipe_rapier_lg.png";
+        ImageButtonItem6.ImageUrl = @"http://media.steampowered.com/apps/dota2/images/items/recipe_rapier_lg.png";
+    }
+
+    public string MatchID { get; set; }
+}
