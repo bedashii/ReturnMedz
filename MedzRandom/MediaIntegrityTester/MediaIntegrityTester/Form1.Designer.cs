b150561458c8f5252922acca30c41bd0ab2b56bf
Rian Mostert <Rianmostert@Hotmail.com>
2013-08-22 15:26:26 +0200

    Chatters updates I don't remember and MediaIntegrityTester which is basically empty.

---
+++ MedzRandom/MediaIntegrityTester/MediaIntegrityTester/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace MediaIntegrityTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.buttonScan = new System.Windows.Forms.Button();
+            this.textBoxConsole = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Path";
+            // 
+            // textBox1
+            // 
+            this.textBoxPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxPath.Location = new System.Drawing.Point(53, 12);
+            this.textBoxPath.Name = "textBox1";
+            this.textBoxPath.Size = new System.Drawing.Size(513, 20);
+            this.textBoxPath.TabIndex = 1;
+            this.textBoxPath.Text = "\\\\MEDZ-PC\\Users\\Medz\\Documents\\Downloads\\complete\\Movies\\Touchy Feely 2013.mkv";
+            // 
+            // buttonScan
+            // 
+            this.buttonScan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonScan.Location = new System.Drawing.Point(572, 12);
+            this.buttonScan.Name = "buttonScan";
+            this.buttonScan.Size = new System.Drawing.Size(75, 23);
+            this.buttonScan.TabIndex = 2;
+            this.buttonScan.Text = "Scan";
+            this.buttonScan.UseVisualStyleBackColor = true;
+            this.buttonScan.Click += new System.EventHandler(this.buttonScan_Click);
+            // 
+            // textBox2
+            // 
+            this.textBoxConsole.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxConsole.Location = new System.Drawing.Point(15, 41);
+            this.textBoxConsole.Multiline = true;
+            this.textBoxConsole.Name = "textBox2";
+            this.textBoxConsole.Size = new System.Drawing.Size(632, 477);
+            this.textBoxConsole.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 530);
+            this.Controls.Add(this.textBoxConsole);
+            this.Controls.Add(this.buttonScan);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonScan;
+        private System.Windows.Forms.TextBox textBoxConsole;
+    }
+}
+
---
+++ MedzRandom/MediaIntegrityTester/MediaIntegrityTester/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaIntegrityTester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        BackgroundWorker backgroundWorkerMKVScan;
+
+        private void buttonScan_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(textBoxPath.Text))
+            {
+                FileInfo fi = new FileInfo(textBoxPath.Text);
+                if (fi.Extension == ".mkv")
+                {
+                    if (backgroundWorkerMKVScan == null)
+                    {
+                        backgroundWorkerMKVScan = new BackgroundWorker();
+                        backgroundWorkerMKVScan.DoWork += delegate
+                        {
+                            Process read = new Process();
+                            read.StartInfo.FileName = "mkvmerge.exe";
+                            read.StartInfo.RedirectStandardOutput = true;
+                            read.StartInfo.RedirectStandardInput = true;
+                            read.StartInfo.UseShellExecute = false;
+                            read.StartInfo.CreateNoWindow = true;
+                            read.StartInfo.Arguments = String.Format(" -o NUL \"{0}\"", textBoxPath.Text);
+
+                            read.Start();
+
+                            StreamReader myStreamReader = read.StandardOutput;
+
+                            int progress = 0;
+
+                            string line = myStreamReader.ReadLine();
+
+                            while (line != null)
+                            {
+                                if (textBoxConsole.InvokeRequired)
+                                {
+                                    textBoxConsole.Invoke((MethodInvoker)delegate
+                                    {
+                                        progress = UpdateProgress(progress, line);
+                                    });
+                                }
+                                else
+                                {
+                                    progress = UpdateProgress(progress, line);
+                                }
+                                line = myStreamReader.ReadLine();
+                            }
+                        };
+
+                        backgroundWorkerMKVScan.RunWorkerCompleted += delegate
+                        {
+                            if (textBoxConsole.InvokeRequired)
+                            {
+                                textBoxConsole.Invoke((MethodInvoker)delegate { textBoxConsole.Text += Environment.NewLine + "DONE!"; });
+                            }
+                            else
+                            {
+                                textBoxConsole.Text += Environment.NewLine + "DONE!";
+                            }
+                        };
+                    }
+                    backgroundWorkerMKVScan.RunWorkerAsync();
+                }
+            }
+
+
+        }
+
+        private int UpdateProgress(int progress, string line)
+        {
+            if (line.StartsWith("Progress"))
+            {
+                progress = int.Parse(line.Substring(10, line.Length - 11));
+                if (progress % 10 == 0)
+                {
+                    if (textBoxConsole.Text == "")
+                        textBoxConsole.Text += line;
+                    else
+                        textBoxConsole.Text += Environment.NewLine + line;
+                }
+            }
+            else
+            {
+                if (textBoxConsole.Text == "")
+                    textBoxConsole.Text += line;
+                else
+                    textBoxConsole.Text += Environment.NewLine + line;
+            }
+            return progress;
+        }
+    }
+}
