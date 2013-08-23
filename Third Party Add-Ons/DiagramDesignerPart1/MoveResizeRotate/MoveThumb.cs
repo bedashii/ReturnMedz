61886d494efbabaf07132ceae6e6186c00eab108
bedasie <lanceb@holistics.co.za>
2013-08-23 18:06:45 +0200

    Added Image Editor and TP Image Editor

---
+++ Third Party Add-Ons/DiagramDesignerPart1/MoveResizeRotate/MoveThumb.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using System;
+
+namespace DiagramDesigner
+{
+    public class MoveThumb : Thumb
+    {
+        public MoveThumb()
+        {
+            DragDelta += new DragDeltaEventHandler(this.MoveThumb_DragDelta);
+        }
+
+        private void MoveThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            ContentControl designerItem = DataContext as ContentControl;
+
+            if (designerItem != null)
+            {
+                Point dragDelta = new Point(e.HorizontalChange, e.VerticalChange);
+
+                RotateTransform rotateTransform = designerItem.RenderTransform as RotateTransform;
+                if (rotateTransform != null)
+                {
+                    dragDelta = rotateTransform.Transform(dragDelta);
+                }
+
+                Canvas.SetLeft(designerItem, Canvas.GetLeft(designerItem) + dragDelta.X);
+                Canvas.SetTop(designerItem, Canvas.GetTop(designerItem) + dragDelta.Y);
+            }
+        }
+    }
+}
