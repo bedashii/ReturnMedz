e17e3453bf5807047df1c997724f788cf5367d0b
Lance Bedasie <dw3lsh@gmail.com>
2017-05-17 21:04:42 +0200

    Created views, added into toolbar

---
+++ ReturnApplications/JIC/Mapper/Mapper/Mapper/Controllers/DetailedViewController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Mapper.Models;
+using Mapper.Models.DBContexts;
+
+namespace Mapper.Controllers
+{
+    public class DetailedViewController : Controller
+    {
+        private CanvasContext db = new CanvasContext();
+
+        // GET: DetailedView
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: DetailedView/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Canvas canvas = db.Canvas.Find(id);
+            if (canvas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(canvas);
+        }
+
+        // GET: DetailedView/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DetailedView/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Location,Map")] Canvas canvas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Canvas.Add(canvas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(canvas);
+        }
+
+        // GET: DetailedView/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Canvas canvas = db.Canvas.Find(id);
+            if (canvas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(canvas);
+        }
+
+        // POST: DetailedView/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Location,Map")] Canvas canvas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(canvas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(canvas);
+        }
+
+        // GET: DetailedView/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Canvas canvas = db.Canvas.Find(id);
+            if (canvas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(canvas);
+        }
+
+        // POST: DetailedView/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Canvas canvas = db.Canvas.Find(id);
+            db.Canvas.Remove(canvas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- ReturnApplications/JIC/Mapper/Mapper/Mapper/Models/DBContexts/CanvasContext.cs
+++ ReturnApplications/JIC/Mapper/Mapper/Mapper/Models/DBContexts/CanvasContext.cs
@@ -5,5 +5,7 @@ namespace Mapper.Models.DBContexts
     public class CanvasContext : DbContext
     {
         public DbSet<CanvasContext> CanvasContexts { get; set; }
+
+        public System.Data.Entity.DbSet<Mapper.Models.Canvas> Canvas { get; set; }
     }
 }
